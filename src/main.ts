03d062dbfdd5a5081c0ed8e2f0dbd96d73176d7e
Jeremy Badger <jeremebadger@gmail.com>
2023-09-01 15:42:09 -0400

    Count number of moves, when counter changes, update <h1> element

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,56 @@
 import './style.css'
 
-const firstListItem = document.querySelector('li')
+let currentPlayer: 'X' | 'O' = 'X'
+let moveCounter = 0
 
-function handleClickSquare(event) {
+// Generic and works for ANY 'li'
+//
+// Because the event tells us the target
+// and the target is the thing we clicked on!
+function handleClickSquare(event: MouseEvent) {
   const thingClickedOn = event.target
 
-  thingClickedOn.textContent = 'X'
+  // If the thing clicked on is an LI Element
+  // - This does several things:
+  // - 1. Checks that the target isn't null
+  // - 2. Lets TypeScript know that *inside* this if statement
+  //      the thingClickedOn is an LI element, and thus we can
+  //      change its textContext
+
+  if (thingClickedOn instanceof HTMLLIElement) {
+    // IF the element ALREADY has 'taken' we shouldn't
+    // let them click again! We do this by ENDING (returning)
+    // from the function RIGHT AWAY!
+    if (thingClickedOn.classList.contains('taken')) {
+      console.log('NO CAN DO!')
+      return
+    }
+
+    thingClickedOn.textContent = currentPlayer
+
+    // Adds the taken class so that we SHOW the user
+    // they can't select this item!
+    thingClickedOn.classList.add('taken')
+
+    if (currentPlayer === 'X') {
+      currentPlayer = 'O'
+    } else {
+      currentPlayer = 'X'
+    }
+
+    // Increment the move counter!
+    moveCounter++
+
+    const header = document.querySelector('h1')
+
+    if (header instanceof HTMLHeadingElement) {
+      header.textContent = `Move ${moveCounter} of Tic Tac Toe`
+    }
+  }
 }
 
-firstListItem?.addEventListener('click', handleClickSquare)
+const allSquares = document.querySelectorAll('li')
+
+allSquares.forEach((square) =>
+  square.addEventListener('click', handleClickSquare)
+)
