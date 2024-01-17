5f660029a0cf49a13c36134ebc2b2f17cf857652
Jeremy Badger <jeremebadger@gmail.com>
2024-01-16 18:06:32 -0800

    Event Bubbling

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ let currentPlayer: 'X' | 'O' = 'X'
 let moveCounter = 0
 
 function handleClickSquare(event: MouseEvent) {
+  console.log(event)
   const thingClickedOn = event.target
 
   if (thingClickedOn instanceof HTMLLIElement) {
     if (thingClickedOn.classList.contains('taken')) {
-      window.alert('Sorry! This square is already taken!')
+      console.log('Sorry! This square is already taken!')
       return
     }
 
@@ -26,12 +27,19 @@ function handleClickSquare(event: MouseEvent) {
   moveCounter++
 
   const header = document.querySelector('h1')
+
   if (header instanceof HTMLHeadingElement)
     header.textContent = `Move ${moveCounter} of Tic Tac Toe`
 }
 
-const allSquares = document.querySelectorAll('li')
+// const allSquares = document.querySelectorAll('li')
 
-allSquares.forEach((square) =>
-  square.addEventListener('click', handleClickSquare)
-)
+// allSquares.forEach((square) =>
+//   square.addEventListener('click', handleClickSquare)
+// )
+
+const gameBoard = document.querySelector('ul')
+
+if (gameBoard instanceof HTMLElement) {
+  gameBoard.addEventListener('click', handleClickSquare)
+}
